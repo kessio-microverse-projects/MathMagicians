3fbcd429861b6dce239f5ec593aff04578663bfc
kessio <64524758+kessio@users.noreply.github.com>
2022-09-08 08:14:28 +0300

    Refactor calculator.js to use hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,59 @@
-import React, { Component } from 'react';
 import DisplayValues from './DisplayValues';
 import calculate from '../logic/calculate'; /* eslint-disable */
+import { useState } from 'react';
 
-export default class Calculator extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      operation: '',
-      next: '',
-    };
-  }
+import React from 'react'
 
+const Calculator = () => {
+
+  const [obj, setCalculations] = useState({total: null, next: null, operation: null});
+     
   clickOperations = (event) => {
-    this.setState((obj) => calculate(obj, event.target.value));
+    setCalculations(calculate(obj, event.target.value))
   }
-
-  render() {
-    const { total, operation, next } = this.state;
-    return (
-      <div>
-        <div className="appCalculator">
-          <div className="grid-container">
-            <div className="screen-input">
-              <span className="text-display">
-                <DisplayValues total={total} operation={operation} next={next} />
-              </span>
-            </div>
-          </div>
-          <div className="grid-container">
-            <input type="button" onClick={this.clickOperations} value="AC" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="+/-" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="%" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="÷" className="same-size-btn orange" />
-          </div>
-          <div className="grid-container">
-            <input type="button" onClick={this.clickOperations} value="7" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="8" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="9" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} className="same-size-btn orange" value="x" />
-          </div>
-          <div className="grid-container">
-            <input type="button" onClick={this.clickOperations} value="4" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="5" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="6" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="-" className="same-size-btn orange" />
-          </div>
-          <div className="grid-container">
-            <input type="button" onClick={this.clickOperations} value="1" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="2" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="3" className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="+" className="same-size-btn orange" />
-          </div>
-          <div className="grid-container">
-            <input type="button" onClick={this.clickOperations} className="zero" value="0" />
-            <input type="button" onClick={this.clickOperations} value="." className="same-size-btn" />
-            <input type="button" onClick={this.clickOperations} value="=" className="same-size-btn orange" />
-          </div>
+  
+  return (
+    <div>
+    <div className="appCalculator">
+      <div className="grid-container">
+        <div className="screen-input">
+          <span className="text-display">
+            <DisplayValues total={total} operation={operation} next={next} />
+          </span>
         </div>
       </div>
-    );
-  }
+      <div className="grid-container">
+        <input type="button" onClick={this.clickOperations} value="AC" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="+/-" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="%" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="÷" className="same-size-btn orange" />
+      </div>
+      <div className="grid-container">
+        <input type="button" onClick={this.clickOperations} value="7" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="8" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="9" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} className="same-size-btn orange" value="x" />
+      </div>
+      <div className="grid-container">
+        <input type="button" onClick={this.clickOperations} value="4" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="5" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="6" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="-" className="same-size-btn orange" />
+      </div>
+      <div className="grid-container">
+        <input type="button" onClick={this.clickOperations} value="1" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="2" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="3" className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="+" className="same-size-btn orange" />
+      </div>
+      <div className="grid-container">
+        <input type="button" onClick={this.clickOperations} className="zero" value="0" />
+        <input type="button" onClick={this.clickOperations} value="." className="same-size-btn" />
+        <input type="button" onClick={this.clickOperations} value="=" className="same-size-btn orange" />
+      </div>
+    </div>
+  </div>
+  )
 }
+
+export default Calculator
