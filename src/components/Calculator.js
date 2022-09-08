cd7172303e7df74fe48e67468104736ce5ee1307
kessio <64524758+kessio@users.noreply.github.com>
2022-09-08 08:28:41 +0300

    removed this from onClick button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,11 +1,9 @@
+import React from 'react'
 import DisplayValues from './DisplayValues';
 import calculate from '../logic/calculate'; /* eslint-disable-line */
 import { useState } from 'react';
 
-import React from 'react'
-
 const Calculator = () => {
-
   const [obj, setCalculations] = useState({total: null, next: null, operation: null});
      
   clickOperations = (event) => {
@@ -23,33 +21,33 @@ const Calculator = () => {
         </div>
       </div>
       <div className="grid-container">
-        <input type="button" onClick={this.clickOperations} value="AC" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="+/-" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="%" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="÷" className="same-size-btn orange" />
+        <input type="button" onClick={clickOperations} value="AC" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="+/-" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="%" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="÷" className="same-size-btn orange" />
       </div>
       <div className="grid-container">
-        <input type="button" onClick={this.clickOperations} value="7" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="8" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="9" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} className="same-size-btn orange" value="x" />
+        <input type="button" onClick={clickOperations} value="7" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="8" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="9" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} className="same-size-btn orange" value="x" />
       </div>
       <div className="grid-container">
-        <input type="button" onClick={this.clickOperations} value="4" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="5" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="6" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="-" className="same-size-btn orange" />
+        <input type="button" onClick={clickOperations} value="4" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="5" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="6" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="-" className="same-size-btn orange" />
       </div>
       <div className="grid-container">
-        <input type="button" onClick={this.clickOperations} value="1" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="2" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="3" className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="+" className="same-size-btn orange" />
+        <input type="button" onClick={clickOperations} value="1" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="2" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="3" className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="+" className="same-size-btn orange" />
       </div>
       <div className="grid-container">
-        <input type="button" onClick={this.clickOperations} className="zero" value="0" />
-        <input type="button" onClick={this.clickOperations} value="." className="same-size-btn" />
-        <input type="button" onClick={this.clickOperations} value="=" className="same-size-btn orange" />
+        <input type="button" onClick={clickOperations} className="zero" value="0" />
+        <input type="button" onClick={clickOperations} value="." className="same-size-btn" />
+        <input type="button" onClick={clickOperations} value="=" className="same-size-btn orange" />
       </div>
     </div>
   </div>
