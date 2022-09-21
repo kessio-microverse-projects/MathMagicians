9f8165c3208a71b9a6cd2ce4bf9a6cd576222dd2
kessio <64524758+kessio@users.noreply.github.com>
2022-09-21 16:18:13 +0300

    add quote page

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function Quote() {
   return (
-    <div className="sectionContainer">
-      This is Quote Page
+    <div className="quoteSection">
+      Mathematics is not about numbers, equations, computations, or algorithms.
+      It is about understanding. -William Paul Thurston
     </div>
   );
 }
