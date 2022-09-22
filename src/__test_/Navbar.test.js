6ef8a053f2007845804bc1a0e2d9a475bbac828f
kessio <64524758+kessio@users.noreply.github.com>
2022-09-22 06:54:09 +0300

    Navbar component snapshot test

---
+++ src/__test_/Navbar.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Navbar from '../components/Navbar';
+
+describe('snapshot test for Home Component', () => {
+it('Matches the snapshot', () => {
+    const tree = renderer.create(<Navbar />).toJSON();
+    expect(tree).toMatchSnapshot('Navbar');
+})
+});
