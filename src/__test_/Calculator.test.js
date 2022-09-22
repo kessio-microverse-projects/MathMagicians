cdf169b27f656985115adcf95afacd0e8c22f100
kessio <64524758+kessio@users.noreply.github.com>
2022-09-22 06:50:19 +0300

    update displavalues and calculator tests

--- src/__test_/Calculator.test.js
+++ src/__test_/Calculator.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Calculator from '../components/Calculator';
 
+describe('snapshot test for Calculator Component', () => {
 it('Matches the snapshot', () => {
     const tree = renderer.create(<Calculator />).toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot('Calculator');
 })
+});
--- src/__test_/DisplayValues.test.js
+++ src/__test_/DisplayValues.test.js
@@ -2,7 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import DisplayValues from '../components/DisplayValues';
 
+describe('snapshot test for DisplayValues Component', () => {
 it('Matches the snapshot', () => {
     const tree = renderer.create(<DisplayValues />).toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot('DisplayVlues');
 })
+});
