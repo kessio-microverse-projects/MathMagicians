ede17335704dcb4f98f6a8437faaef0d973d83f1
kessio <64524758+kessio@users.noreply.github.com>
2022-09-22 12:44:26 +0300

    jest testing on calaculate

--- src/__test_/Calculator.test.js
+++ src/__test_/Calculator.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Calculator from '../components/Calculator';
 
 describe('snapshot test for Calculator Component', () => {
@@ -7,4 +9,22 @@ describe('snapshot test for Calculator Component', () => {
     const tree = renderer.create(<Calculator />).toJSON();
     expect(tree).toMatchSnapshot();
   });
+});
+
+describe('simulate user event', () => {
+  test('click divide', () => {
+    render(<Calculator />);
+    expect(userEvent.click(screen.getByTestId('divide'))).toHaveBeenCalled;
+  });
+  test('click multiply', () => {
+    render(<Calculator />);
+    expect(userEvent.click(screen.getByTestId('multiply'))).toHaveBeenCalled;
+  });
+
+  test('click minus', () => {
+    render(<Calculator />);
+    expect(userEvent.click(screen.getByTestId('minus'))).toHaveBeenCalled;
+  });
+
+
 });
